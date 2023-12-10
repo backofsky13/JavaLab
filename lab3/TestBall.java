f8850b9658e871746a0b5ce3a58ae99d30b9cfcc
backofsky13 <152891123+backofsky13@users.noreply.github.com>
2023-12-10 18:53:04 +0300

    Update TestBall.java

--- lab3/TestBall.java
+++ lab3/TestBall.java
@@ -2,7 +2,7 @@ package lab3;
 
 public class TestBall {
     public static void main(String[] args) {
-        Ball b1 = new Ball(75, 90);
+        Ball b1 = new Ball(57, 99);
         System.out.println(b1);
         b1.move(20, 25);
         System.out.println(b1);
