4875dd55eb6f73de486576b6d857c7cbab5fa514
backofsky13 <152891123+backofsky13@users.noreply.github.com>
2023-12-10 18:18:04 +0300

    Update TestBook.java

--- lab1/TestBook.java
+++ lab1/TestBook.java
@@ -2,9 +2,9 @@ package lab1;
 
 public class TestBook {
     public static void main(String[] args) {
-        Book b1 = new Book("War and Peace", 1300);
-        Book b2 = new Book("The Great Gatsby", 320);
-        Book b3 = new Book("Ann of Green Gables", 384);
+        Book b1 = new Book("Good to Great", 1113);
+        Book b2 = new Book("Prestuplenie and nakazanie", 977);
+        Book b3 = new Book("World", 543);
         System.out.println(b1);
         b1.theHalfPages();
         b2.theHalfPages();
