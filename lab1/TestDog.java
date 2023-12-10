465e8981db56be852dd4e1707db5f50319fcfc14
backofsky13 <152891123+backofsky13@users.noreply.github.com>
2023-12-10 18:16:16 +0300

    Update TestDog.java

--- lab1/TestDog.java
+++ lab1/TestDog.java
@@ -2,9 +2,9 @@ package lab1;
 
 public class TestDog {
     public static void main(String[] args) {
-        Dog d1 = new Dog("Mike", 2);
-        Dog d2 = new Dog("Helen", 7);
-        Dog d3 = new Dog("Bob"); d3.setAge(1);
+        Dog d1 = new Dog("Bonya", 3);
+        Dog d2 = new Dog("Busya", 9);
+        Dog d3 = new Dog("Villi"); d3.setAge(1);
         System.out.println(d1);
         d1.intoHumanAge();
         d2.intoHumanAge();
