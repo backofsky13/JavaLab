db710a9286ec5765438c73b5bdc58414a858eafb
backofsky13 <152891123+backofsky13@users.noreply.github.com>
2023-12-10 21:49:06 +0300

    Update Starter.java

--- lab17/Starter.java
+++ lab17/Starter.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 public class Starter {
     public static void main(String[] args) {
         ArrayList<Student> s = new ArrayList<>();
-        s.add(new Student("Стенков Михаил Антонович", 93, 8, 19));
-        s.add(new Student("Фамилия Имя Отчество", 78, 14, 18));
-        s.add(new Student("Устал придумывать ФИО", 67, 20, 20));
-        s.add(new Student("Надоело уже блин", 93, 8, 19));
+        s.add(new Student("Новиков Петр Сергеевич", 75, 5, 23));
+        s.add(new Student("Романов Тимур Генадьевич", 83, 7, 18));
+        s.add(new Student("Тульский Пряник Двусотович", 57, 3, 20));
+        s.add(new Student("Пиковый Туз Козырневыч", 79, 13, 21));
         new LabClassUI(s);
     }
 }
